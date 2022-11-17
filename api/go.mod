9c40f896928ad86da80e7ae393766e773278e609
George Spanos <gespa11019@gmail.com>
2022-11-17 19:04:57 +0200

    initialize code

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/George-Spanos/mastermind-api
+
+go 1.19
---
+++ ui/android/app/src/main/kotlin/com/example/mastermind_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mastermind_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
