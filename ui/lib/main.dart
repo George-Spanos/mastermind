21e3e3daf81dc6959256cfd9c07c7ccc2de6938a
George Spanos <gespa11019@gmail.com>
2022-11-29 22:25:34 +0200

    dynamicly build config

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mastermind_ui/app_initializer.dart';
 import 'package:mastermind_ui/game/game/game.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: kDebugMode ? ".env" : "release.env");
 
   runApp(AppInitializer(child: const MastermindApp()));
 }
